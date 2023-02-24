fd02e671e794ac7d370bb315f1294725537f59a5
AliceChpmnk <alisablog50@gmail.com>
2023-02-24 20:12:34 +0200

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import { GlobalStyle } from './GlobalStyle';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+  
+  handleBtnClick = (e) => {
+    const { name } = e.target;
+    this.setState({ [name]: this.state[name] + 1 });
+  }
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + bad + neutral;
+  }
+
+  countPositiveFeedbackPercentage = (total) => {
+    if (this.state.good === 0) {
+      return 0;
+    }
+
+    return parseInt(this.state.good / total * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage(total);
+    const options = ['good', 'neutral', 'bad'];
+
+    return (
+      <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={this.handleBtnClick} />
+        </Section>
+      <Section title="Statistics">
+        <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />
+      </Section>
+      <GlobalStyle />
+      </>
+    )
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyledButton } from './FeedbackOptions.styled'
+import PropTypes from 'prop-types';
+
+export default function FeedbackOptions({ options, onLeaveFeedback}) {
+  return (
+    <div>
+          {options.map(option => {
+              return <StyledButton key={ option } name={option} onClick={onLeaveFeedback}>{option}</StyledButton>
+          })}
+    </div>
+  )
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+}
